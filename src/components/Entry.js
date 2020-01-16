9bb11f0fad9cd48d92dd27f3832339448259be9d
Ashleigh Christopher <ashleighc207@gmail.com>
2020-01-16 14:42:24 -0500

    add in main page styles

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,31 +1,41 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "../styles/EntryStyles.css";
 
 const Entry = () => {
   const [zip, setZip] = useState("");
   const [name, setName] = useState("");
 
   return (
-    <div>
-      <h2>Welcome to your custom weather report!</h2>
-      <p>
+    <div className="main-container">
+      <h2 className="heading">Welcome to your custom weather report!</h2>
+      <p className="description">
         This app uses local storage in your browser to save your name and zip
         code to check the weather. If you clear your storage, you will need to
         re-enter your information for your forecast.
       </p>
-      <label>
+      <label className="label" htmlFor="name">
         Name:
-        <input
-          type="text"
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
       </label>
-      <label>
+      <input
+        className="input"
+        type="text"
+        value={name}
+        id="name"
+        onChange={e => setName(e.target.value)}
+      />
+      <label className="label" htmlFor="zip">
         Zip Code:
-        <input type="text" value={zip} onChange={e => setZip(e.target.value)} />
       </label>
+      <input
+        className="input"
+        type="text"
+        value={zip}
+        id="zip"
+        onChange={e => setZip(e.target.value)}
+      />
       <Link
+        className="button"
         to={{
           pathname: "/Home",
           name: name,
