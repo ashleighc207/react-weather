ded2b6a62d973ea9b6e8f841bd4b5b970ef464d2
Ashleigh Christopher <ashleighc207@gmail.com>
2020-01-16 12:55:02 -0500

    save date formatting

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,20 +5,21 @@ import { useLocation } from "react-router";
 
 const Homepage = ({ props }) => {
   const { name, zip } = useLocation();
-  console.log(name, zip);
+
+  const [firstName, setFirstName] = useState(
+    localStorage.getItem("name") || ""
+  );
+  const [zipCode, setZipCode] = useState(localStorage.getItem("zip") || "");
+  const [data, setData] = useState({});
+  const [dateArr, setDateArr] = useState([]);
+
   const getWeather = async zip => {
     try {
-      if (!localStorage.getItem("weatherData")) {
-        // let response = await axios.get(
-        //   `https://api.openweathermap.org/data/2.5/forecast?zip=${zip}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-        // );
-        // setData(response.data);
-        // localStorage.setItem("weatherData", JSON.stringify(response.data));
-        // console.log(JSON.stringify(response, null, 2));
-      } else {
-        let wd = JSON.parse(localStorage.getItem("weatherData"));
-        console.log(wd);
-      }
+      let response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&units=imperial&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+      );
+      await setData(response);
+      return response;
     } catch (err) {
       console.log(err);
     }
@@ -29,16 +30,92 @@ const Homepage = ({ props }) => {
 
     if (!isCancelled) {
       getWeather(21234);
+      if (zip) {
+        setZipCode(zip);
+        localStorage.setItem("zip", zip);
+      }
+      if (name) {
+        setFirstName(name);
+        localStorage.setItem("name", name);
+      }
     }
     return () => {
       isCancelled = true;
     };
   }, []);
 
+  const generateDayGroups = () => {
+    let newArray = [];
+    for (var date in data) {
+      newArray.push(data[date]);
+    }
+    setDateArr(newArray);
+    return newArray;
+  };
+
+  useEffect(() => {
+    let isCancelled = false;
+    if (!isCancelled) {
+      if (data.data) {
+        const groups = data.data.list.reduce((groups, date) => {
+          const datePoint = new Date(date.dt * 1000).getDate();
+          if (!groups[datePoint]) {
+            groups[datePoint] = [];
+          }
+          groups[datePoint].push(date);
+          return groups;
+        }, {});
+        setData(groups);
+        console.log(groups);
+      }
+      if (!data.data) {
+        generateDayGroups();
+      }
+    }
+    return () => {
+      isCancelled = true;
+    };
+  }, [data]);
+
+  useEffect(() => {
+    console.log(dateArr);
+  }, [dateArr]);
+
   return (
-    <div class="main night">
-      <div>Welcome, {name}</div>
-      <p>The weather today is </p>
+    <div className="main night">
+      <div>Welcome, {firstName}</div>
+      <p>The weather today is x in {zipCode}</p>
+      {dateArr &&
+        dateArr.map(d => {
+          return (
+            <div className="border">
+              {d.map(date => {
+                return (
+                  <div key={date.dt}>
+                    <span>
+                      {`${new Date(date.dt * 1000).toLocaleDateString("en-US", {
+                        weekday: "short",
+                        month: "short",
+                        day: "numeric"
+                      })}`}{" "}
+                    </span>
+                    <span>
+                      {`${
+                        new Date(date.dt * 1000).getHours() < 12
+                          ? new Date(date.dt * 1000).getHours()
+                          : new Date(date.dt * 1000).getHours() - 12
+                      }:${
+                        new Date(date.dt * 1000).getMinutes() < 10 ? "0" : ""
+                      }${new Date(date.dt * 1000).getMinutes()} ${
+                        new Date(date.dt * 1000).getHours() < 12 ? " AM" : " PM"
+                      }`}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
     </div>
   );
 };
